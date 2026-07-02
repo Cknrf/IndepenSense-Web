fc9cb54d733f7fc5a290db43292b630527128167
cknrf <mearckfrancisvoughnlol@gmail.com>
2026-07-02 09:46:04 +0800

    add: contact section

--- IndepenSense-Web/src/App.tsx
+++ IndepenSense-Web/src/App.tsx
@@ -196,6 +196,87 @@ function App() {
 
           </div>
 
+          <div className="contact-section section">
+            <div className="stack-container">
+              <div className="message-container">
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                  <path d="M0 0h24v24H0z" fill="none" />
+                  <path fill="currentColor" d="M6 17c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6m9-9a3 3 0 0 1-3 3a3 3 0 0 1-3-3a3 3 0 0 1 3-3a3 3 0 0 1 3 3M3 5v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2" />
+                </svg>
+
+                <h2><span>Rowela</span>'s Guardian</h2>
+              </div>
+            </div>
+            <div className="stack-container">
+              <div className="summary-container">
+                <div className="svg-container">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                    <path d="M0 0h24v24H0z" fill="none" />
+                    <path fill="currentColor" d="M16 17v2H2v-2s0-4 7-4s7 4 7 4m-3.5-9.5A3.5 3.5 0 1 0 9 11a3.5 3.5 0 0 0 3.5-3.5m3.44 5.5A5.32 5.32 0 0 1 18 17v2h4v-2s0-3.63-6.06-4M15 4a3.4 3.4 0 0 0-1.93.59a5 5 0 0 1 0 5.82A3.4 3.4 0 0 0 15 11a3.5 3.5 0 0 0 0-7" />
+                  </svg>
+
+                </div>
+                <div className="monitored-container">
+                  <p>User:</p>
+                  <span>Rowela</span>
+                </div>
+                <div className="line"></div>
+                <div className="guardian-count-container">
+                  <p>Total Guardian:</p>
+                  <span>4</span>
+                </div>
+              </div>
+            </div>
+            <div className="stack-container">
+              <div className="contact-list-container">
+                <div className="contact-container">
+                  <div className="picture-container"></div>
+                  <div className="contact-information">
+                    <p className="name">Juan Dela Cruz</p>
+                    <p className="role">Primary Guardian/Husband</p>
+                    <p className="number">09482648292</p>
+                  </div>
+
+                </div>
+
+                 <div className="contact-container">
+                  <div className="picture-container"></div>
+                  <div className="contact-information">
+                    <p className="name">Juan Dela Cruz</p>
+                    <p className="role">Primary Guardian/Husband</p>
+                    <p className="number">09482648292</p>
+                  </div>
+
+                </div>
+
+                 <div className="contact-container">
+                  <div className="picture-container"></div>
+                  <div className="contact-information">
+                    <p className="name">Juan Dela Cruz</p>
+                    <p className="role">Primary Guardian/Husband</p>
+                    <p className="number">09482648292</p>
+                  </div>
+
+                </div>
+
+                 <div className="contact-container">
+                  <div className="picture-container"></div>
+                  <div className="contact-information">
+                    <p className="name">Juan Dela Cruz</p>
+                    <p className="role">Primary Guardian/Husband</p>
+                    <p className="number">09482648292</p>
+                  </div>
+
+                </div>
+
+               
+
+              </div>
+            </div>
+
+          </div>
+
 
 
 
