a2914381205288220cc1b596ed9c41a9f282fba0
cknrf <mearckfrancisvoughnlol@gmail.com>
2026-07-04 15:07:44 +0800

    fix: svg not showing

--- IndepenSense-Web/src/components/Signin/Signin.tsx
+++ IndepenSense-Web/src/components/Signin/Signin.tsx
@@ -17,7 +17,7 @@ function Signin({ onSetActiveSection }: SetActiveSection) {
               <path d="M0 0h24v24H0z" fill="none" />
               <path
                 fill="currentColor"
-                d="M12 22q-3.475-.875`-`5.738-3.988T4 11.1V5l8-3l8 3v6.1q0 3.8-2.262 6.913T12 22"
+                d="M12 22q-3.475-.875-5.738-3.988T4 11.1V5l8-3l8 3v6.1q0 3.8-2.262 6.913T12 22"
               />
             </svg>
           </div>
