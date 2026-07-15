da23def590fdaec5bfae110fd609679ccdfc3c74
cknrf <mearckfrancisvoughnlol@gmail.com>
2026-07-15 19:40:47 +0800

    fix: convert SVG props to camelCase to silence React warnings

--- IndepenSense-Web/src/components/Home/HomeSection.tsx
+++ IndepenSense-Web/src/components/Home/HomeSection.tsx
@@ -68,11 +68,11 @@ function HomeSection() {
                 <g
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                 >
-                  <path stroke-dasharray="20" d="M3 12h17.5">
+                  <path strokeDasharray="20" d="M3 12h17.5">
                     <animate
                       fill="freeze"
                       attributeName="stroke-dashoffset"
@@ -81,8 +81,8 @@ function HomeSection() {
                     />
                   </path>
                   <path
-                    stroke-dasharray="12"
-                    stroke-dashoffset="12"
+                    strokeDasharray="12"
+                    strokeDashoffset="12"
                     d="M21 12l-7 7M21 12l-7 -7"
                   >
                     <animate
